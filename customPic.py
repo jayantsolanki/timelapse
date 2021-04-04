9c49292576f55001886188f8d05d3901acbbc2d9
Jayant Solanki <jayantjnp@gmail.com>
2021-04-04 13:36:43 -0700

    ready for cronjob

--- customPic.py
+++ customPic.py
@@ -2,14 +2,14 @@ import os, os.path
 from picamera import PiCamera
 
 dir = "./photos"
-countofImages = len(next(os.walk(dir))[2]) #dir is your directory path as string
+countofImages = len(next(os.walk(dir))[2]) 
 print (countofImages)
-# print('image{0:04d}.jpg'.format(countofImages))
+
 camera = PiCamera()
 camera.resolution = (1024, 768)
 camera.start_preview()
 
-camera.capture('image{0:04d}.jpg'.format(countofImages))
+camera.capture('./photos/image{0:04d}.jpg'.format(countofImages))
 # camera.capture('image.jpg')
 camera.stop_preview()
 print("Done")  
