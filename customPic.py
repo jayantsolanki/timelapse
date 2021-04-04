63540eb9e81b28f8ae42b321a87a26523a2091ce
Jayant Solanki <jayantjnp@gmail.com>
2021-04-04 14:18:42 -0700

    misc

--- customPic.py
+++ customPic.py
@@ -1,15 +1,20 @@
 import os, os.path
 from picamera import PiCamera
+import datetime
 
 dir = "./photos"
+dateraw= datetime.datetime.now()
+datetimeformat = dateraw.strftime("%Y-%m-%d_%H:%M")
 countofImages = len(next(os.walk(dir))[2]) 
-print (countofImages)
+
+print("RPi took next photo for yuour timelapse: " + datetimeformat)
+print ('Total Images captured {d}'.format(countofImages+1))
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
 camera.start_preview()
 
-camera.capture('./photos/image{0:04d}.jpg'.format(countofImages))
+camera.capture('./photos/image{0:06d}.jpg'.format(countofImages))
 # camera.capture('image.jpg')
 camera.stop_preview()
 print("Done")  
